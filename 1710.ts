2b0148a15d9457c7ecab61dedb1efa639722edee
yasuda0320 <65007448+yasuda0320@users.noreply.github.com>
2021-07-09 17:36:21 +0900

    [JS][TS] 1710. Maximum Units on a Truck

---
+++ 1710.ts
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} boxTypes
+ * @param {number} truckSize
+ * @return {number}
+ */
+function maximumUnits(boxTypes: number[][], truckSize: number): number {
+    boxTypes.sort((a, b) => b[1] - a[1])
+    let total = 0
+    for (let [boxes, units] of boxTypes) {
+        total += units * (boxes <= truckSize ? boxes : truckSize)
+        truckSize -= boxes
+        if (truckSize <= 0)
+            break
+    }
+    return total
+}
+
+console.log(maximumUnits([[1,3],[2,2],[3,1]], 4) == 8)
+console.log(maximumUnits([[5,10],[2,5],[4,7],[3,9]], 10) == 91)
