7f8685034cbc6349c0a21af7029c7a57450fdd5c
yasuda0320 <65007448+yasuda0320@users.noreply.github.com>
2021-07-16 22:52:33 +0900

    [Rust] 1480. with iter().enumerate()

---
+++ 1480_iter.rs
@@ -0,0 +1,22 @@
+struct Solution {}
+
+impl Solution {
+  pub fn running_sum(nums: Vec<i32>) -> Vec<i32> {
+    let mut sums = vec![0; nums.len()];
+    for (i, n) in nums.iter().enumerate() {
+      sums[i] += n + (if i > 0 { sums[i - 1] } else { 0 } );
+    }
+    sums
+  }
+}
+
+fn main() {
+  let result = Solution::running_sum(vec![1,2,3,4]);
+  println!("{} {:?}", result == [1,3,6,10], result);
+  
+  let result = Solution::running_sum(vec![1,1,1,1,1]);
+  println!("{} {:?}", result == [1,2,3,4,5], result);
+  
+  let result = Solution::running_sum(vec![3,1,2,10,1]);
+  println!("{} {:?}", result == [3,4,6,16,17], result);
+}
