b3d7ac06f65b99dd23e60481c74429794ca23335
yasuda0320 <65007448+yasuda0320@users.noreply.github.com>
2023-08-06 17:30:22 +0900

    片方がなくなった時点でbreakするように修正

--- 1768.go
+++ 1768.go
@@ -5,9 +5,15 @@ func mergeAlternately(word1 string, word2 string) string {
   for i := 0; i < len1 || i < len2; i++ {
     if i < len1 {
       result += string(word1[i])
+    } else {
+      result += string(word2[i:])
+      break
     }
     if i < len2 {
       result += string(word2[i])
+    } else {
+      result += string(word1[i+1:])
+      break
     }
   }
   return result
