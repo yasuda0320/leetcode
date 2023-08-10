88f8a8cae58895bce43eb65a70478f727fdb6710
yasuda0320 <65007448+yasuda0320@users.noreply.github.com>
2023-08-11 04:24:29 +0900

    C++版をコンパクトに

--- 605.cpp
+++ 605.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int count = 0;
-        int i = 0;
-        int size = flowerbed.size();
-        
-        while (i < size) {
+        for (int i = 0; i < flowerbed.size() && n > 0; i++) {
             if (flowerbed[i] == 0 &&
-                (i == 0 || flowerbed[i - 1] == 0) &&
-                (i == size - 1 || flowerbed[i + 1] == 0)) {
-                
-                flowerbed[i] = 1;
-                count++;
+                    (i == 0 || flowerbed[i - 1] == 0) &&
+                    (i == flowerbed.size() - 1 || flowerbed[i + 1] == 0)) {
+                n--;
+                i++; // Skip adjacent plot
             }
-            if (count >= n) {
-                return true;
-            }
-            i++;
         }
-        return false;
+        return n <= 0;
     }
 };
