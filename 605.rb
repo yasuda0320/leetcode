fa4053141fb29350f493f0ea8f43b1d60a58961b
yasuda0320 <65007448+yasuda0320@users.noreply.github.com>
2023-08-11 11:45:27 +0900

    Ruby版をシンプルに

--- 605.rb
+++ 605.rb
@@ -2,18 +2,15 @@
 # @param {Integer} n
 # @return {Boolean}
 def can_place_flowers(flowerbed, n)
-  flowerbed += [0, 0]
-  count = 0
   i = 0
-
-  while i < flowerbed.size - 2
-    if flowerbed[i] == 0 && flowerbed[i - 1] == 0 && flowerbed[i + 1] == 0
-      count += 1
-      i += 2
-    else
-      i += 1
+  while i < flowerbed.length && n > 0
+    if flowerbed[i] == 0 &&
+        (i == 0 || flowerbed[i - 1] == 0) &&
+        (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)
+      n -= 1
+      i += 1 # Skip adjacent plot
     end
-    return true if count >= n
+    i += 1
   end
-  false
+  n <= 0
 end
